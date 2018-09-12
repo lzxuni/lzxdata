e42aa0cb559309c85a668008847ce968734030ce
zth-1 <45952745@qq.com>
2018-09-12 15:02:53 +0800

    实时视频修复更新Bug

--- src/main/java/com/lzxuni/modules/system/mapper/realTimeVideo/RealTimeVideoMapper.java
+++ src/main/java/com/lzxuni/modules/system/mapper/realTimeVideo/RealTimeVideoMapper.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface RealTimeVideoMapper extends BaseMapper<RealTimeVideo> {
     List<RealTimeVideo> queryList(RealTimeVideo realTimeVideo);
     List<RealTimeVideo> findmohuList(RealTimeVideo realTimeVideo);
+    RealTimeVideo findByPhotoId(String id);
 }
